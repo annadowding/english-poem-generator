26a2e281ec0219ecc0489e013df84856bc153874
Anna Dowding <144890388+annadowding@users.noreply.github.com>
2023-12-12 13:09:48 +0000

    Increasing typewriter speed

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function displayPoem(response) {
   new Typewriter("#poem-box", {
     strings: response.data.answer,
     autoStart: true,
-    delay: 10,
+    delay: 88,
     cursor: "",
   });
 }
@@ -13,7 +13,7 @@ function generatePoem(event) {
   let context =
     "You are interested in English folklore and fairytales. You enjoy writing poetry with lts of imagery from the nature you see in the ENglish countryside. Your favourite book is Jane Eyre by Charlotte Bronte. You like to imagine being Robin Hood in your free time. Your favouite poem is Goblin Market by Christina Rosetti";
 
-  let prompt = `Please write me a 4 line poem about ${userEntry.value}. Each line of the poem must begin on a new line using a <br/>. Sign off every poem with <strong>by SheCodes AI 💖</strong>`;
+  let prompt = `Please write me a 4 line poem about ${userEntry.value}. Each line of the poem must begin on a new line using a <br/>. Sign off every poem with <strong>by SheCodes AI 💖</strong> which must be on a separete line by itself`;
   let apiKey = "c695b4fc90b605eea29b70ecbaft3f9o";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   let poem = document.querySelector("#poem-box");
